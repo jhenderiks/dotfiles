e6034329d3250c306ebe0fac04ee0859315d53b7
Justin Henderiks <jhenderiks@users.noreply.github.com>
2025-03-19 10:31:32 -0400

    some lint

--- modules/common/shell/default.nix
+++ modules/common/shell/default.nix
@@ -1,18 +1,9 @@
 { pkgs, ... }:
 
 {
-  imports = [
-    ./starship
-    ./bash.nix
-    ./direnv.nix
-    ./fish.nix
-    ./git.nix
-  ];
+  imports = [ ./starship ./bash.nix ./direnv.nix ./fish.nix ./git.nix ];
 
   environment.shellAliases.k = "kubectl";
 
-  environment.systemPackages = [
-    pkgs.neofetch
-    pkgs.nixpkgs-fmt
-  ];
+  environment.systemPackages = [ pkgs.neofetch ];
 }
