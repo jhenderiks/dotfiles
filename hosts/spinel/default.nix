bfc07a36eda2569a684f66f26af2b20f58a2caed
Justin Henderiks <jhenderiks@users.noreply.github.com>
2026-04-19 16:18:48 +0300

    enable t3code on spinel

--- hosts/spinel/default.nix
+++ hosts/spinel/default.nix
@@ -58,13 +58,13 @@ inputs.nixpkgs.lib.nixosSystem {
         keepassxc.enable = true;
         ledger-live.enable = true;
         # nordvpn.enable = true; # TODO: switch to surfshark / proton
-        opencode-desktop.enable = true;
         slack.enable = true;
         spotify.enable = true;
         steam.enable = true;
         # sunshine.enable = true;
         syncthing.enable = true;
         telegram.enable = true;
+        t3code.enable = true;
         zoom.enable = true;
       }
     )
