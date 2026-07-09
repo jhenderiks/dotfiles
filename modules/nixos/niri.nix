7a2c76a0924bd8f4989599061d13e7b5fd776a84
Justin Henderiks <jhenderiks@users.noreply.github.com>
2026-07-09 14:51:21 -0400

    Set Niri default window width to one sixth

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -393,7 +393,7 @@
                   { proportion = 1.0 / 2.0; }
                   { proportion = 2.0 / 3.0; }
                 ];
-                default-column-width.proportion = 1.0 / 3.0;
+                default-column-width.proportion = 1.0 / 6.0;
                 border = lib.mkForce {
                   enable = true;
                   width = 1;
